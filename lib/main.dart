bc7e1c3a48c14d2578bffb5de581b0ba42a4a30d
Alessandro Cataldi <alessandro.cataldi@outlook.com>
2022-05-25 12:40:26 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:thememode/provider/theme_provider.dart';
+import 'package:thememode/widget/change_theme_button.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //     debugShowCheckedModeBanner: false,
+  //     title: 'Flutter Demo',
+  //     themeMode: ThemeMode.system,
+  //     theme: CustomTheme.lightTheme,
+  //     darkTheme: CustomTheme.darkTheme,
+  //     home: const MyHomePage(title: "Title"),
+  //   );
+  // }
+
+  @override
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+        create: (context) => ThemeProvider(),
+        builder: (context, _) {
+          final themeProvider = Provider.of<ThemeProvider>(context);
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Demo',
+            themeMode: themeProvider.themeMode,
+            theme: CustomTheme.lightTheme,
+            darkTheme: CustomTheme.darkTheme,
+            home: const MyHomePage(title: "Title"),
+          );
+        },
+      );
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    //  indentify theme mode
+    final text = MediaQuery.of(context).platformBrightness == Brightness.dark
+        ? "Dark Theme"
+        : "Light Theme";
+
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        centerTitle: false,
+        title: Text(widget.title),
+        actions: [
+          ChangeThemeButtonWidget(),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Scaffold = $text"),
+          ],
+        ),
+      ),
+    );
+  }
+}
